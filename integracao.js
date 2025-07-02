6a9de1f8faab72d09fcfa2e470c21582b01eb160
Eduardo3007 <eduardo.oliveira@elastic.fit>
2025-07-02 12:29:20 -0300

    quarto commit

--- integracao.js
+++ integracao.js
@@ -21,6 +21,24 @@ joystickAlturaRotacao = nipplejs.create({
 joystickAlturaRotacao.on('move', function(event, data) {
     EstadoJoystickAlturaRotacao.Direcao = data.angle?.degree;
     EstadoJoystickAlturaRotacao.Forca = data.distance;
+
+    //envia os dados coletados pelo joystick para a controladora(ESP8266)
+    fetch('/joystickAlturaRotacao', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            EstadoJoystickAlturaRotacao: EstadoJoystickAlturaRotacao
+        })
+    })
+    .then(response => response.text())
+    .then(respostaDoServidor => {
+        console.log('Resposta do servidor: ', respostaDoServidor)
+    })
+    .catch(error => {
+        console.error('Erro: ', error);
+    })
 });
 
 //configuracao do joystick direcoes
@@ -35,6 +53,65 @@ joystickDirecoes = nipplejs.create({
 joystickDirecoes.on('move', function(event, data) {
     EstadoJoystickDirecoes.Direcao = data.angle?.degree;
     EstadoJoystickDirecoes.Forca = data.distance;
+
+    //envia os dados coletados pelo joystick para a controladora(ESP8266)
+    fetch('/joystickDirecoes', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            EstadoJoystickDirecoes: EstadoJoystickDirecoes
+        })
+    })
+    .then(response => response.text())
+    .then(respostaDoServidor => {
+        console.log('Resposta do servidor: ', respostaDoServidor)
+    })
+    .catch(error => {
+        console.error('Erro: ', error);
+    })
 })
 
+
+ //endpoints para funcoes do drone
+  botaoLigarDesligar.addEventListener("click", function(){
+      fetch("/ligarDesligar").then(response => {
+        if(response.ok){
+          console.log("Comando ligar-desligar enviado")
+        }
+      });
+  });
+
+  botaoDecolar.addEventListener("click", function(){
+      fetch("/decolar").then(response => {
+          if(response.ok){
+              console.log("Comando decolar enviado")
+          }
+      })
+  })
+
+  botaoPousar.addEventListener("click", function() {
+      fetch("/pousar").then(response => {
+          if(response.ok) {
+              console.log("comando pousar enviado")
+          }
+      })
+  })
+
+  botaoVoltarCasa.addEventListener("click", function(){
+      fetch("/casa").then(response => {
+          if(response.ok) {
+              console.log("Comando voltar para casa enviado")
+          }
+      })
+  })
+
+  botaoSoltarCarga.addEventListener("click", function() {
+      fetch("/soltarCarga").then(response => {
+          if(response.ok) {
+              console.log("Comando solcar carga enviado")
+          }
+      })
+  })
 
